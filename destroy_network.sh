eaf5a871a6d38f70a7e150b1bc9663959322cac4
Navpreet Singh <54878321+navpreet-debut@users.noreply.github.com>
2022-05-22 16:48:46 +0530

    Update destroy_network.sh

--- destroy_network.sh
+++ destroy_network.sh
@@ -3,4 +3,4 @@ docker rm -f $(docker ps -aq)
 docker network prune 
 docker volume prune
 docker rmi -f $(docker images | awk '($1 ~ /dev-peer.*.nlts.*/) {print $3}')
-# rm -rf ../KoreChainSDK/wallet/
+rm -rf ../sdk/wallet/
